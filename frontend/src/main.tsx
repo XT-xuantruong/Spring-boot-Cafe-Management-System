8a602d2170bb51061a3b7653d76161bd1e947a0e
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-04-21 19:57:56 +0700

    Feat: Add sidebar support with new CSS variables and integrate SidebarProvider in main component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,13 +6,16 @@ import { Provider } from 'react-redux'
 import { persistor, store } from './stores/index.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter } from 'react-router-dom'
+import { SidebarProvider } from './components/ui/sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
+          <SidebarProvider>
             <App />
+          </SidebarProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
