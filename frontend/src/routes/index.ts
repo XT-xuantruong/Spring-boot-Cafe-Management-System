1892682b2ad2c82af42f4108e37ae37dae575905
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-04-21 17:30:21 +0700

    Feat: Add initial route definitions for public and private routes

---
+++ frontend/src/routes/index.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = [
+  //   { path: "/login", component: LoginPage, layout: AuthLayout },
+  //   { path: "/register", component: RegisterPage, layout: AuthLayout },
+  //   { path: "/otp", component: OtpFormPage, layout: AuthLayout },
+];
+export const privateRoutes = [
+  //   { path: "/", component: Home, layout: MainSidebarLayout },
+  //   { path: "/profile/:id", component: ProfilePage, layout: AuthLayout },
+  //   { path: "/messenger", component: MessengerPage, layout: MessengerLayout },
+  //   { path: "/search", component: SearchResult, layout: MainSidebarLayout },
+];
