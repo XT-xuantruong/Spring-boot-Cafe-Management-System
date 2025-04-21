cd6f57f0a7fbdd7f615c5ce482e02b1523de0b5c
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-04-21 18:16:20 +0700

    Feat: Add public route for HomePage with UserLayout

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,10 +1,13 @@
+import UserLayout from "@/layouts/UserLayout";
+import HomePage from "@/pages/HomePage";
+
 export const publicRoutes = [
   //   { path: "/login", component: LoginPage, layout: AuthLayout },
   //   { path: "/register", component: RegisterPage, layout: AuthLayout },
   //   { path: "/otp", component: OtpFormPage, layout: AuthLayout },
+  { path: "/", component: HomePage, layout: UserLayout },
 ];
 export const privateRoutes = [
-  //   { path: "/", component: Home, layout: MainSidebarLayout },
   //   { path: "/profile/:id", component: ProfilePage, layout: AuthLayout },
   //   { path: "/messenger", component: MessengerPage, layout: MessengerLayout },
   //   { path: "/search", component: SearchResult, layout: MainSidebarLayout },
